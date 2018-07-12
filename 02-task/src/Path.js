f41336f4a586cfd667e2e6eee61b92e6d4a6eeca
Ihar Shavel <Ihar_Shavel@epam.com>
2018-07-12 19:44:21 +0300

    it works for one line

--- 02-task/src/Path.js
+++ 02-task/src/Path.js
@@ -1,30 +1,25 @@
 class Path {
-    constructor(iDelay) {
-        this.delay = iDelay;
+    constructor(svgLink, frequency) {
+        // this.delay = iDelay;
         this.data = [];
+        this.frequency = frequency;
+        this.svgPath = svgLink;
     }
 
-    initPath() {
-        var path = this.svg.append('path')
-            .attr('fill', 'none')
-            .attr('stroke-width', '2')
-            // .attr('d', line(this.dataArray));
-            
-        return path;
-    }
-
-    updatePath(path, value) {
+    render(value, line) {
         // console.log("render Path");
         this.data.push(value);
-        this.timeArray.push(new Date());
+        // console.log("PUSH VALUE");
+        // this.timeArray.push(new Date());
 
-        var line = d3.line()
-            .x((d, i) => {return this.startXMargin + this.x(this.timeArray[i])})
-            .y((d, i) => {return 800 + this.startYMargin - d*800});
+        // var line = d3.line()
+        //     .x((d, i) => {return this.x(this.timeArray[i])})
+        //     .y((d, i) => {return 800 + this.startYMargin - d*800}); //800
     
-        this.x.domain([this.startDate, new Date()]);
-        path.transition()
-            .duration(1000)///NOT 1000
+        // this.x.domain([this.startDate, new Date()]);
+        // console.log('AZAZAZAZZAZAZAZAZAZA');
+        this.svgPath.transition()
+            .duration(this.frequency)
             .ease(d3.easeLinear)
             .attr("d", line(this.data));
     }
--- 02-task/src/emitter.js
+++ 02-task/src/emitter.js
@@ -1,25 +1,33 @@
 const { from, interval } = rxjs;
 const { map, flatMap} = rxjs.operators;
 
-let svgHandler = new SvgHandler();
+let chart = new Chart(new Date, 800);
 
 let pathOneDelay = 1000;
 
-svgHandler.initializeAxes();
-let pathOne = new Path(pathOneDelay);
-// let pathOne = svgHandler.initializePath(pathOneDelay);
+chart.initializeAxes();
+let pathOne = new Path(chart.initializePath("orange", 2), pathOneDelay);
+let lineFunction = chart.getLine();
+// let pathOne = chart.initializePath(pathOneDelay);
 
 let axis$ = interval(Math.max(300, pathOneDelay)).pipe(map(() => Math.random()));
 let data$ = interval(pathOneDelay).pipe(map(() => Math.random()));
 
 data$.subscribe(
-    (val) => svgHandler.renderPath(pathOne,val),
+    (value) => {
+        
+        chart.updateXDomain();
+        chart.updateTime();
+        pathOne.render(value, lineFunction);
+        // console.log('mapsdi');
+       
+    }, 
     () => console.log("error"),
     () => console.log("completed")
 );
 
 axis$.subscribe(
-    () => svgHandler.renderXAxis(),
+    () => chart.renderXAxis(),
     () => console.log("error"),
     () => console.log("completed")
 );
